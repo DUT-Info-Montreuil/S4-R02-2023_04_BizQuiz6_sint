5f14dbd5988e24329a4d8ab27131a4295ede26ec
PriyankSolanki <psolanki@condorcet93.fr>
2023-04-11 13:44:38 +0200

    questionnaire

--- test/fr/iut/montreuil/S4_R02_2023_4/BizQuiz6/sint/impl/test2.java
+++ test/fr/iut/montreuil/S4_R02_2023_4/BizQuiz6/sint/impl/test2.java
@@ -1,5 +1,7 @@
 package fr.iut.montreuil.S4_R02_2023_4.BizQuiz6.sint.impl;
 
-public class test2 {
+import fr.iut.montreuil.R4_S02_2023_4.BizQuiz6_questionnaire_sme.entities.dto.QuestionDto;
 
+public class test2 {
+	QuestionDto q = new QuestionDto(0, 0, null, null, null, 0, null, null);
 }
