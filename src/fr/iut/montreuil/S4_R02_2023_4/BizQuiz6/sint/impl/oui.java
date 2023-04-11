75531c29dcefdb9cedfcc611be015d6975aa2d80
PriyankSolanki <psolanki@condorcet93.fr>
2023-04-11 15:42:01 +0200

    test dans la class oui

--- src/fr/iut/montreuil/S4_R02_2023_4/BizQuiz6/sint/impl/oui.java
+++ src/fr/iut/montreuil/S4_R02_2023_4/BizQuiz6/sint/impl/oui.java
@@ -2,6 +2,7 @@ package fr.iut.montreuil.S4_R02_2023_4.BizQuiz6.sint.impl;
 
 import fr.iut.montreuil.R4_S02_2023_4.BizQuiz6_questionnaire_sme.entities.bo.QuestionnaireBO;
 import fr.iut.montreuil.R4_S02_2023_4.BizQuiz6_questionnaire_sme.entities.dto.QuestionDto;
+import fr.iut.montreuil.S4_R02_2023_4.BizQuiz6.sint.entities.dto.StatsQuestionnaireDTO;
 
 public class oui {
 
@@ -19,6 +20,40 @@ public class oui {
 		}
 		questionnaire.addQuestion(q1);
 		questionnaire.addQuestion(q2);
+		
+		System.out.println(recupererStatsQuestionnaire(questionnaire).toString());
 	}
+	
+	
+	public static StatsQuestionnaireDTO recupererStatsQuestionnaire(QuestionnaireBO questionnaire) {
+		QuestionDto questionPlus = questionnaire.getListeDeQuestion().get(0);
+		double ratioPlus = questionPlus.getStatsQuestionDTO().getNbCorrecte() / questionPlus.getStatsQuestionDTO().getNbJouerQuestion();
+		QuestionDto questionMoin = questionnaire.getListeDeQuestion().get(0);
+		double ratioMoin = questionMoin.getStatsQuestionDTO().getNbCorrecte() / questionMoin.getStatsQuestionDTO().getNbJouerQuestion();
+		
+		for (QuestionDto q : questionnaire.getListeDeQuestion()) {
+			double ratio = q.getStatsQuestionDTO().getNbCorrecte() / q.getStatsQuestionDTO().getNbJouerQuestion();
+			
+			if(ratio > ratioPlus) 
+				questionPlus = q;
+			else if (ratio == ratioPlus) {
+				if(q.getDifficulte() > questionPlus.getDifficulte()) 
+					questionPlus = q;
+				else if(q.getDifficulte() == questionPlus.getDifficulte() && q.getStatsQuestionDTO().getNbJouerQuestion() > questionPlus.getStatsQuestionDTO().getNbJouerQuestion()) 
+					questionPlus = q;				
+			}
+			if(ratio < ratioMoin) 
+				questionMoin = q;
+			else if (ratio == ratioMoin) {	
+				if(q.getDifficulte() < questionMoin.getDifficulte()) 
+					questionMoin = q;
+				else if(q.getDifficulte() == questionMoin.getDifficulte() && q.getStatsQuestionDTO().getNbJouerQuestion() > questionPlus.getStatsQuestionDTO().getNbJouerQuestion()) 
+					questionPlus = q;
+			}
+		}
+		StatsQuestionnaireDTO stats= new StatsQuestionnaireDTO(questionnaire.getNbJouerQuestionnaire(), questionPlus, questionMoin, questionnaire.getIdQuestionnaire());
+		return stats;
+	}
+	
 
 }
